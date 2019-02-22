15029b10b9ae81170ac6582ac1535360f2336e16
Parsa Rostami <Par3a@outlook.com>
2019-02-22 16:21:34 +0330

    Update 5.RecursiveFunc.py

--- Session5/5.RecursiveFunc.py
+++ Session5/5.RecursiveFunc.py
@@ -5,4 +5,22 @@ def power(a,b):
         return power(a, b-1) * a
 
 
+def power1(a,b):
+    if b == 1:
+        return a
+    else:
+        if b % 2 == 0:
+            x = power1(a, b //2)
+            return x * x
+        else:
+            return power1(a, b // 2) * power1(a, b // 2 + 1)
+
+def power2(a,b):
+    if b == 1:
+        return a
+    else:
+        return power2(a, b // 2) * power2(a, b - b // 2)
+
 print(power(2,3))
+print(power1(2,3))
+print(power2(2,3))

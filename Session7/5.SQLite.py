2bff80f51faca792c5483b2bff33e195dd9c9c30
Parsa Rostami <Par3a@outlook.com>
2019-03-08 18:04:32 +0330

    Update Session7 files

---
+++ Session7/5.SQLite.py
@@ -0,0 +1,15 @@
+# import sqlite3
+# conn = sqlite3.connect('my_db.db')
+# c = conn.cursor()
+# c.execute("CREATE TABLE phone (name text, phone text)")
+# c.execute("INSERT INTO phone VALUES ('John', '123456')")
+# conn.commit()
+# conn.close()
+
+
+import sqlite3
+conn = sqlite3.connect('my_db.db')
+c = conn.cursor()
+c.execute('SELECT name, phone FROM phone')
+print(c.fetchall())
+conn.close()
